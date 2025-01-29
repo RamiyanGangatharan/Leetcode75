baf7721e10fb2f44fcaad345e6a816ffd4785802
Ramiyan Gangatharan <ramiyan2185@gmail.com>
2025-01-28 19:52:38 -0500

    LC1768

---
+++ LeetCode75/LC1768.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace LeetCode75
+{
+    /// <summary>
+    /// You are given two strings word1 and word2. 
+    /// Merge the strings by adding letters in 
+    /// alternating order, starting with word1. 
+    /// If a string is longer than the other, 
+    /// append the additional letters onto the 
+    /// end of the merged string. 
+    /// 
+    /// Return the merged string.
+    /// </summary>
+    internal class LC1768
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            StringBuilder result = new();
+
+            int i = 0;
+            int j = 0;
+
+            // Loop through both strings checking whether one string is longer then the other
+            while (i < word1.Length || j < word2.Length)
+            {
+                if (i < word1.Length) { _ = result.Append(word1[i]); i++; }
+                if (j < word2.Length) { _ = result.Append(word2[j]); j++; }
+            }
+
+            return result.ToString(); // merged string
+        }
+    }
+}
---
+++ LeetCode75/Program.cs
@@ -0,0 +1,8 @@
+﻿using LeetCode75;
+
+LC1768 problem_1 = new LC1768();
+
+string word1 = "1357";
+string word2 = "2468";
+
+Console.WriteLine(problem_1.MergeAlternately(word1, word2));
